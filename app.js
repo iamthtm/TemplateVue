1a1900cff4fda7707d8c13f392995f163d7a8372
iamthtm <p_1624_n@hotmail.com>
2016-08-27 11:59:36 +0700

    end

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ var app = new Vue({
   el: '#app',
   data: {
     vuejs: 'Hello Vue.js!'
+  },
+  methods: {
+    getApi : function () {
+      var url = 'https://sheetsu.com/apis/v1.0/a56386b650d1'
+      this.$http.get(url).then(function (res) {
+        console.log(res)
+      })
+
+    }
+  },
+  ready: function () {
+    this.getApi()
   }
+
 })
 console.log(app)
